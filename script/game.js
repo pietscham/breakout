645f030dfe1c554bf5531fac90f0c9e5a4fa6b53
Annaliese Pietsch <apietsch@vailsys.com>
2018-03-15 22:19:30 -0500

    add collision check for vertical lines

--- script/game.js
+++ script/game.js
@@ -3,9 +3,9 @@
 var balls;
 var canvas;
 var ballCanvas;
-var lineDirection = "y";
+var lineDirection = 'y';
 var lines;
-
+var limits;
 
 
 function Line(x, y) {
@@ -13,20 +13,26 @@ function Line(x, y) {
   this.y = y;
   this.upLimit = y;
   this.downLimit = y;
-  this.direction = 'y';
+  this.leftLimit = x;
+  this.rightLimit = x;
+  this.direction = lineDirection;
   this.speed = 15;
   this.iter = 1;
   this.width = 15;
   this.height = 15;
-  this.color = "#FFFFFF";
+  this.color = "#333333";
   
-  this.changeDirection = function() {
-    this.direction = this.direction == 'y' ? 'x' : 'y'; 
-  },
   this.expand = function() {
-    this.upLimit += (this.speed);
-    this.downLimit -= this.speed;
+    if(this.direction == 'y') {
+        this.downLimit += (this.speed);
+        this.upLimit -= this.speed;
+    }
+    else {
+        this.leftLimit -= (this.speed);
+        this.rightLimit += this.speed;
+    }
   }
+
 }
 
 
@@ -64,10 +70,32 @@ function Ball(x, y) {
   this.setSpeed = function(givenSpeed) {
     this.speed = givenSpeed;
   };
-  this.updateLimit = function() {
-    this.rightLimit = ballCanvas.width;
-    this.lowerLimit = ballCanvas.height;
+  this.updateLimitResize = function(prevWidth, prevHeight) {
+    if(this.rightLimit == prevWidth) {
+        this.rightLimit = ballCanvas.width;
+    }
+    if(this.lowerLimit == prevHeight) {
+        this.lowerLimit = ballCanvas.height;
+    }
   };
+  this.updateLimitLine = function(line) {
+    if(line.direction == 'y') {
+        if(this.x < line.x) {
+            this.rightLimit = Math.min(line.x,this.rightLimit);
+        }
+        else if(this.rightLimit != line.x){
+            this.leftLimit = Math.max(line.x, this.leftLimit);
+        }
+    }
+    else {
+        if(this.y < line.y) {
+            this.lowerLimit = Math.max(line.y, this.lowerLimit);
+        }
+        else if(this.lowerLimit != line.y) {
+            this.upperLimit = Math.min(line.y, this.upperLimit);
+        }
+    }
+  }
 }
 
 function getRandomDirection() {
@@ -102,7 +130,8 @@ function initGame(canvas, ballCanvas, lineCanvas) {
     this.ballCtx = ballCanvas.getContext("2d");
     this.lineCtx = lineCanvas.getContext("2d");
     this.balls = [new Ball(15, 15), new Ball(this.ballCanvas.width/2, 1+10), new Ball(100, 200)];
-    this.lines = [new Line(40, 40)]
+    this.lines = []
+    limits = []
 }
 
 function mainLoop() {
@@ -133,25 +162,61 @@ function drawBalls() {
 
 function drawLines() {
     for(let l of lines) {
-        lineCtx.beginPath();
-        lineCtx.rect(l.x, l.y, l.width, l.upLimit - l.y);
-        lineCtx.fillStyle = "#000000";
-        lineCtx.closePath();
-        lineCtx.fill();
-        lineCtx.beginPath();
-        lineCtx.rect(l.x, l.y, l.width, l.downLimit - l.y);
-        lineCtx.fillStyle = "#000000";
-        lineCtx.closePath();
-        lineCtx.fill();
-        
+        if(l.direction == 'y') {
+            drawVerticalLine(l);
+        }
+        else {
+            drawHorizontalLine(l);
+        }
         l.expand();
     }
     lineCtx.save();
 }
 
+function drawVerticalLine(l) {
+    if(l.upLimit >= lineCanvas.height && l.downLimit <= 0) {
+        lineCtx.rect(l.x, l.y, l.width, lineCanvas.height);
+    }
+    else {
+        for(let b of balls) {
+            b.updateLimitLine(l);
+        }
+    }
+    lineCtx.beginPath();
+    lineCtx.rect(l.x, l.y, l.width, l.upLimit - l.y);
+    lineCtx.fillStyle = l.color;
+    lineCtx.closePath();
+    lineCtx.fill();
+    lineCtx.beginPath();
+    lineCtx.rect(l.x, l.y, l.width, l.downLimit - l.y);
+    lineCtx.fillStyle = l.color;
+    lineCtx.closePath();
+    lineCtx.fill();
+}
+
+function drawHorizontalLine(l) {
+    if(l.leftLimit <= 0 && l.rightLimit >= lineCanvas.width) {
+        lineCtx.rect(l.x, l.y, lineCanvas.width, l.height);
+    }
+    else {
+        for(let b of balls) {
+            b.updateLimitLine(l);
+        }
+    }
+    lineCtx.beginPath();
+    lineCtx.rect(l.x, l.y, l.leftLimit - l.x, l.height);
+    lineCtx.fillStyle = l.color;
+    lineCtx.closePath();
+    lineCtx.fill();
+    lineCtx.beginPath();        
+    lineCtx.rect(l.x, l.y, l.rightLimit - l.x, l.height);   
+    lineCtx.fillStyle = l.color;
+    lineCtx.closePath();
+    lineCtx.fill();
+}
+
 function clearBalls() {
     ballCtx.clearRect(0, 0, ballCanvas.width, ballCanvas.height);
-    lineCtx.clearRect(0, 0, lineCanvas.width, lineCanvas.height);
 }
 
 function rightClick(ev) {
@@ -159,6 +224,7 @@ function rightClick(ev) {
     var pos = getMousePos(ev);
     ctx.beginPath();
     var l = new Line(pos.x, pos.y);
+    lines.push(l);
     return false;
 }
 
@@ -179,15 +245,16 @@ function getMousePos(e) {
 }
 
 function resize() {
+    prevWidth = this.canvas.width;
+    prevHeight = this.canvas.height;
     this.canvas.height = window.innerHeight;
     this.canvas.width = window.innerWidth;
     this.ballCanvas.height = window.innerHeight;
     this.ballCanvas.width = window.innerWidth;
     this.lineCanvas.height = window.innerHeight;
     this.lineCanvas.width = window.innerWidth;
-    this.lineCanvas.restore();
     for (let b of balls) {
-        b.updateLimit();
+        b.updateLimitResize(prevWidth, prevHeight);
     }
     
 }
